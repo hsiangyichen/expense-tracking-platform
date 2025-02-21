0463b88ad60a61166915b788e8b0765f984be3bf
hsiangyichen <hsiangyi1025@gmail.com>
2025-02-20 21:01:32 -0800

    feat: Update layout components and styles for improved user experience and branding

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -13,9 +14,28 @@ export default async function RootLayout({
     redirect("/");
   }
 
+  const user = {
+    id: loggedIn?.id ?? "",
+    firstName: loggedIn?.firstName ?? "Guest",
+    lastName: loggedIn?.lastName ?? "",
+    email: loggedIn?.emailAddresses?.[0]?.emailAddress ?? "",
+  };
+
   return (
     <main className="flex h-screen w-full">
-      <div className="flex size-full flex-col">{children}</div>
+      <div className="flex size-full flex-col">
+        <div className="flex h-16 items-center justify-between p-5 shadow-creditCard sm:p-8 md:hidden">
+          <Image
+            src="/icons/logo.svg"
+            alt="logo"
+            width={200}
+            height={30}
+            className="w-28"
+          />
+          <div>{user.firstName}</div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
